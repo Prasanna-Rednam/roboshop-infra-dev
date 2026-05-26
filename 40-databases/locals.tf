3732c478213cb3b4673117218eebe2d4c23ea0f8
Prasanna Rednam <prasannarednam08@gmail.com>
2026-05-27 02:23:43 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,7 +2,7 @@ locals {
     ami_id = data.aws_ami .joindevops.id
     # public subnet in 1a AZ
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    mongodb_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 
     common_tags = {
     Project = var.project
